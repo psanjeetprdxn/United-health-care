18d182eb2fde3e0fb8987916912a92ef74447e53
psanjeetprdxn <sanjeetp@prdxn.com>
2018-11-29 19:19:11 +0530

    php: added function to get all presentaion details

---
+++ classes/Presentation.php
@@ -0,0 +1,29 @@
+<?php
+
+class Presentation extends Connection
+{
+    protected $conn;
+    public function __construct()
+    {
+        $connection = new Connection;
+        $this->conn = $connection->connect();
+    }
+
+    /*
+    *************************************************************************************************************************
+         RETURNS PRESENTAION DETAILS FROM PRESENTAION TABLE IN MULTIDIMENSIONAL ARRAY (NAME, CLIENT NAME, STATUS, ETC)
+    *************************************************************************************************************************
+    */
+
+    public function getPresentation()
+    {
+        $presentations = array();
+        $presentationQuery = "SELECT * FROM presentation";
+        $presentation = $this->conn->prepare($presentationQuery);
+        $presentations = $presentation->fetchAll();
+        return $presentations;
+    }
+}
+
+
+?>
