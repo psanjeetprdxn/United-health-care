d727e21bd91292d6166cf55f8558638a03f64c44
psanjeetprdxn <sanjeetp@prdxn.com>
2018-11-29 16:02:20 +0530

    html: header structure added

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!-- start of header -->
+<div class="wrapper">
+  <div class="header-top">
+    <h1>
+      <a href="index.php" title="United Health Care">
+        <img src=../assets/images/logo.png alt="United Health Care">
+        United Health Care
+      </a>
+    </h1>
+    <div class="header-generator">
+      <span class="capitalize">value story generator</span>
+    </div><!-- end of header-generator -->
+  </div><!-- end of header-top -->
+  <div class="header-bottom">
+    <nav>
+      <ul>
+        <!-- menu for non-login users (starts)-->
+        <li>
+          <a class="capitalize" href="login.php" title="Login">login</a>
+        </li>
+        <li>
+          <a class="capitalize" href="signup.php" title="Sign Up">Sign Up</a>
+        </li>
+        <!-- menu for non-login users (end)-->
+        <!-- menu for login users (starts) -->
+        <li>
+          <span class="uppercase" title="#FIXME">Username</a>
+        </li>
+        <li>
+          <a class="capitalize" href="includes/logout.php" class="logout.php">log out</a>
+        </li>
+        <!-- menu for login users (ends) -->
+        <!-- menu for all types of users (starts)-->
+        <li>
+          <a class="capitalize" href="#FIXME" title="Help">Help +</a>
+        </li>
+        <!-- menu for all types of users (ends)-->
+      </ul>
+    </nav>
+  </div><!-- end of header-bottom -->
+</div><!-- end of header wrapper -->
+<!-- end of header -->
