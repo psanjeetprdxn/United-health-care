fb53971c6317ff611d7dff0cea85f48e628b97bc
psanjeetprdxn <sanjeetp@prdxn.com>
2018-12-03 12:30:12 +0530

    php: added conditional statement in presentation table to show particular column to particular users

--- index.php
+++ index.php
@@ -25,65 +25,79 @@
 			<!-- php function (getPresentation) to get all the presentation details -->
 			<?php
 			function __autoload($classname)
-					{
-							include "classes/$classname.php";
-					}
-					$presentation = new Presentation;
-					$presentations = $presentation->getPresentation();
-			 ?>
+			{
+				include "classes/$classname.php";
+			}
+			$presentation = new Presentation;
+			$presentations = $presentation->getPresentation();
+			?>
 			 <!-- end of getPresentation() function -->
 			<!-- start of presentation section -->
-            <div class="presentation">
-              <div class="wrapper">
-                <h3 class="capitalize">create, edit or delete a presentation below</h3>
-                <div class="new-presentation">
-                    <a href="#FIXME" title="Create New Presentaion">create new presentaion</a>
-                </div>
-                <?php
-                if (!empty($presentations)) {
-                ?>
-                <div class="presentation-details">
-                    <!-- PRESENTAION COLUMN NAME -->
-                    <div class="table-row-header">
-                        <div class="table-header"><strong>Name</strong></div>
-                        <div class="table-header"><strong>Client Name</strong></div>
-                        <div class="table-header"><strong>Date Created</strong></div>
-                        <div class="table-header"><strong>Date Updated</strong></div>
-                        <div class="table-header"><strong>Edit, Delete or clone</strong></div>
-                        <div class="table-header"><strong>Status</strong></div>
-                        <div class="table-header"><strong>Type</strong></div>
-                    </div>
-                    <!-- end of presentaion column name -->
-                    <!-- PRESENTAION DATA -->
-                    <?php
-                    foreach ($presentations as $ppt) {
-                    ?>
-                    <div class="table-row-data">
-                        <div class="table-data"><span><?php echo $ppt['name'] ?></span></div>
-                        <div class="table-data"><span><?php echo $ppt['client_name'] ?></span></div>
-                        <div class="table-data"><span><?php echo date('d-m-Y', strtotime($ppt['date_created'])); ?></span></div>
-                        <div class="table-data"><span><?php echo date('d-m-Y', strtotime($ppt['date_edited'])); ?></span></div>
-                        <div class="table-data">
-                            <a class="edit" href="#FIXME" title="Update">Update</a>
-                            <a class="delete" href="delete.php?presentation_id=<?php echo $ppt['presentation_id']; ?>" title="Delete">Delete</a>
-                        </div>
-                        <div class="table-data"><span><?php echo $ppt['status'] ?></span></div>
-                        <div class="table-data"><span><?php echo $ppt['type'] ?></span></div>
-                    </div>
-                    <?php
-                    }//end of foreach loop
-                    ?>
-                    <!-- end of presentation data -->
-                </div><!-- end of presentation details -->
-                    <?php
-                } else {
-                    ?>
-                    <span class="presentaion-message">No data to show</span>
-                    <?php
-                }//end of else
-                ?>
-              </div>
+      <div class="presentation">
+        <div class="wrapper">
+          <h3 class="capitalize">create, edit or delete a presentation below</h3>
+          <div class="new-presentation">
+              <a href="#FIXME" title="Create New Presentaion">create new presentaion</a>
+          </div>
+          <?php
+          if (!empty($presentations)) {
+          ?>
+          <div class="presentation-details">
+            <!-- PRESENTAION COLUMN NAME -->
+            <div class="table-row-header">
+              <div class="table-header"><strong>Name</strong></div>
+              <div class="table-header"><strong>Client Name</strong></div>
+              <div class="table-header"><strong>Date Created</strong></div>
+              <div class="table-header"><strong>Date Updated</strong></div>
+							<?php
+							if (isset($_SESSION['user_id'])) {
+							?>
+								<div class="table-header"><strong>Edit, Delete or clone</strong></div>
+							<?php
+							}
+							?>
+              <div class="table-header"><strong>Status</strong></div>
+              <div class="table-header"><strong>Type</strong></div>
             </div>
+            <!-- end of presentaion column name -->
+            <!-- PRESENTAION DATA -->
+            <?php
+            foreach ($presentations as $ppt) {
+            ?>
+            <div class="table-row-data">
+              <div class="table-data"><span><?php echo $ppt['name'] ?></span></div>
+              <div class="table-data"><span><?php echo $ppt['client_name'] ?></span></div>
+              <div class="table-data"><span><?php echo date('d-m-Y', strtotime($ppt['date_created'])); ?></span></div>
+              <div class="table-data"><span><?php echo date('d-m-Y', strtotime($ppt['date_edited'])); ?></span></div>
+							<?php
+							if (isset($_SESSION['user_id'])) {
+							?>
+              <div class="table-data">
+                <a class="edit" href="#FIXME" title="Update">Update</a>
+								<?php if ($_SESSION['role'] == 'admin') { ?>
+                <a class="delete" href="delete.php?presentation_id=<?php echo $ppt['presentation_id']; ?>" title="Delete">Delete</a>
+								<?php } ?>
+              </div>
+							<?php
+							}
+							?>
+              <div class="table-data"><span><?php echo $ppt['status'] ?></span></div>
+              <div class="table-data"><span><?php echo $ppt['type'] ?></span></div>
+            </div>
+              <?php
+              }//end of foreach loop
+              ?>
+              <!-- end of presentation data -->
+          </div><!-- end of presentation details -->
+          <?php
+          } else {
+          ?>
+          <span class="presentaion-message">No data to show</span>
+          <?php
+          }//end of else
+          ?>
+        </div>
+      </div>
 			<!-- end of presentation section -->
 		</main>
 		<!-- End of main -->
